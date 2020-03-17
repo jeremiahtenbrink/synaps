242fde64047c781e37bc3db830142d42e58f70b8
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2020-03-16 21:11:12 -0500

    Adding new SVG's to see if thos animations will work.

--- src/images/svgAnimation/CardAnimation.js
+++ src/images/svgAnimation/CardAnimation.js
@@ -5,15 +5,17 @@ import { genPercentAdd } from "antd/lib/upload/utils"
 
 function SvgComponent({background = "transparent", cardTop = "transparent", width = 60, height = 81, card1 ="black", card2 = "pink"}) {
     return (
+        
         <svg width={width} height={height} viewBox="0 0 20 27" fill="none">
+                <LeftCard>
                 <path fill= {cardTop} d="M3.275 3l16.186.204-.275 21.85L3 24.85z" />
-            
+                </LeftCard>
                 <path
                 stroke="#F6F5F3" fill ={card1}
                 d="M3.769 3.506l15.186.191-.263 20.85-15.186-.19z"/>     
-            <LeftCard>
+            
             <path fill={card1} d="M0 3.152L16.196 3l.205 21.865-16.196.152z" />
-            </LeftCard>
+        
             <div>
                 <path
                 stroke= "white" fill= {card2}
@@ -27,7 +29,7 @@ function SvgComponent({background = "transparent", cardTop = "transparent", widt
 }
 const LeftCard = styled.div`
     transform: rotateX(-45deg);
-    border: 2px solid green;
+    border: 2px solid red;
     transition: all 4s;
 `;
 const rightCard = styled.div`
